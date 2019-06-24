522b23bff96a9f8b26914162bcf7ad0026d68c0e
LIGHT\J.Qi <j.qi@osram.com>
2019-06-23 21:56:19 -0400

    added more comments about callback, promises, and Async/Await funcitons. Used arrow function, and tried array.map and array.forEach and found solution to use array with async/await funcitons.

--- library.js
+++ library.js
@@ -9,25 +9,64 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
 
 // "Synchronously" means "using the same clock" so when two instructions are synchronous they use the same clock and must happen one after the other. 
 // "Asynchronous" means "not using the same clock" so the instructions are not concerned with being in step with each other. 
-// Synchronous/Asynchronous HAS NOTHING TO DO WITH MULTI-THREADING.
-(async function example() {
-  // var is function scoped and let is block scoped.
-  let driver = await new Builder().forBrowser('chrome').build(); 
+// Synchronous/Asynchronous does NOT equal to single threaded/multi-threaded. In order to be multi-theraded, you need to have multiprocessor machines. 
+// Asynchronous does NOT run programs simultaneously, it halt programs' execution until they need attention, and the computer can execute other things in the meantime. 
+// Starting with ES6, JavasScript introduced Promises(ES2015) and Async/Await(ES2017) to help with asynchronous code that do not involve using callbacks:
+// https://www.youtube.com/watch?v=PoRJizFvM7s
+async function test() {
+  // var is function scoped and let is block scoped. 
+  // in JavaScript, variable and functions are all variables, const can not change, let can change.
   // build is an async funtion. 
-  // use await to make sure it is fully build before js is going to run the next line
+  // use await to make sure it is fully build before js is going to run the next line, it is like smart wait.
+  let driver = await new Builder().forBrowser('chrome').build();
+  
   try {
     // tryCode - Block of code to try
     await driver.get('https://housepricehub.com/');
+
     // search field
-    await driver.findElement(By.css('input')).then(function(el){
-      console.log("success " + el);
-    });
-    // top menu items
-    await driver.findElements(By.css('#app .top-menu-wrap:nth-of-type(1) .top-menu a')).then(function(array){
-      console.log("found the elements you wanted " + array);
-    });
+    // ES2015: promises:
+    // use ".then" function from Javascript promises to pass in the placeholder text to log it in the console
+    // ES6 style arrow funtion: .then(function(txt)){} is the same as .then((txt) =>){}
+    // driver.findElement(By.css('input')).getAttribute("placeholder").then((txt) => {
+    //   console.log("The text is " + txt);
+    // });
+    // ES2017: async/await:
+    // findElement is a promise (take time to return).
+    const element = await driver.findElement(By.css('input')); 
+    if (element) {
+      const txt = await element.getAttribute("placeholder");
+      // if you see promise in the print out, such as "The text is [object Promise]"", then you must missed an await.
+      console.log("The text is " + txt);
+    }
+    else {
+      console.log('Can not find the element');
+      exit;
+    }
+
+    // top menu items - find multiple Elements at once
+    // await driver.findElements(By.css('#app .top-menu-wrap:nth-of-type(1) .top-menu a')).then(function(array){
+    //   console.log("found the elements you wanted: " + array);
+    // });
+    // // getText() only work for single element, won't work for an array of elements.
+    // await driver.findElement(By.css('#app .top-menu-wrap:nth-of-type(1) .top-menu a:nth-of-type(1)')).getText().then(function(txt){
+    //   console.log("found the txt you wanted: " + txt);
+    // });
+    const elements = await driver.findElements(By.css('#app .top-menu-wrap:nth-of-type(1) .top-menu a'));
+    // array.map and array.forEach are used to call back functions, when using async funciton inside you will encounter problems.
+    //const elementsTexts = elements.map(async function(element) {return await element.getText()} );
+    // elementsTexts.forEach((item) => {
+    //   console.log("The text of the top menu item element is: " + item);  
+    // });
+    for (const element of elements) {
+      const txt = await element.getText();
+      console.log("The text of the top menu item element is: " + txt);
+    }
+
   } finally {
     // finallyCode - Block of code to be executed regardless of the try / catch result
     await driver.quit();
   }
-})();
+};
+
+test();
